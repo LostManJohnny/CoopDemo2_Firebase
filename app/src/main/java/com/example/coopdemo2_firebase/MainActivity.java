2fd1e3867239b01923005038163bb17bb50231f3
LostManJohnny <mrmatias1997@gmail.com>
2022-10-05 15:46:09 -0500

    Started implementing google sign in feature

--- app/src/main/java/com/example/coopdemo2_firebase/MainActivity.java
+++ app/src/main/java/com/example/coopdemo2_firebase/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.coopdemo2_firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
+
+    ImageButton google;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        google = findViewById(R.id.imbtn_Google);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn(){
+        BeginSignInRequest signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.google_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
     }
 }
